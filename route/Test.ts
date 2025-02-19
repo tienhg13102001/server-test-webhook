abff7b6e4db5664cc2b1dcb746b4f2161b00928c
Tiến <tienhg201@gmail.com>
2025-02-19 11:58:42 +0700

    done

--- route/Test.ts
+++ route/Test.ts
@@ -1,11 +1,23 @@
 import express, { Express, Request, Response } from "express";
-import dotenv from "dotenv";
+
 
 const Router = express.Router();
 
 Router.get("/", (req: Request, res: Response) => {
-  res.send("<h1>OKh1>");
+  res.send("<h1>OK </h1>");
+});
+
+Router.post("/test", (req: Request, res: Response) => {
+  fetch(`https://api.telegram.org/${process.env.BOT_TOKEN}/sendMessage`, {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify({
+      text: "oke",
+      chat_id: 2004808521,
+    }),
+  });
 });
 
 export { Router as testRouter };
-
--- route/Webhook.ts
+++ route/Webhook.ts
@@ -1,28 +1,24 @@
 import express from "express";
 import { Request, Response } from "express";
 import axios from "axios";
+import "dotenv/config";
 
 const Route = express.Router();
 
 // Token của bot Telegram
 const BOT_TOKEN = process.env.BOT_TOKEN;
-const TELEGRAM_API = `https://api.telegram.org/bot${BOT_TOKEN}`;
+const TELEGRAM_API = `https://api.telegram.org/${BOT_TOKEN}`;
 
 Route.post("/webhook", async (req: Request, res: Response) => {
-  const { message } = req.body;
+  const { chat_id } = req.body;
+  const text = "Chào mừng bạn đến với Mini App!";
 
-  if (message && message.text) {
-    const chatId = message.chat.id;
-    const text = "Chào mừng bạn đến với Mini App!";
+  const resAPI = await axios.post(`${TELEGRAM_API}/sendMessage`, {
+    chat_id,
+    text,
+  });
 
-    // Gửi tin nhắn phản hồi
-    await axios.post(`${TELEGRAM_API}/sendMessage`, {
-      chat_id: chatId,
-      text: text,
-    });
-  }
-
-  res.status(200).send("OK");
+  res.status(200).json(resAPI.data);
 });
 
 export { Route as webhookRoute };
