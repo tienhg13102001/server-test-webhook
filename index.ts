c97d55553bebe58861c5ac2522996d32cb5fa3f3
Tiến <tienhg201@gmail.com>
2025-02-19 12:29:57 +0700

    done

--- index.ts
+++ index.ts
@@ -1,11 +1,14 @@
 import express, { Express, Request, Response } from "express";
 import dotenv from "dotenv";
 import { appRouter } from "./route";
+import cors from 'cors';
 
 dotenv.config();
 
 const PORT = process.env.PORT || 3000;
 const app: Express = express();
+
+app.use(cors());
 
 app.use(express.json());
 
